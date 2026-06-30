b476f3c76b65a23e3ce290c45e6bff2844d65f86
Its EKD <eliwaindah@gmail.com>
2026-06-30 17:24:43 +0300

    Uploading images and contact

--- src/data/profile/contact.js
+++ src/data/profile/contact.js
@@ -1,9 +1,9 @@
 const contact = {
   email: "info@gigo.co.ke",
 
-  phone: "+254 253 338 89",
+  phone: "+254 7253 338 89",
 
-  whatsapp: "+254 253 338 89",
+  whatsapp: "+254 7253 338 89",
 
   location: "Kakamega, Kenya",
 };
